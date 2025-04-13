9dae002b858c78f83b8e3b903cc458e3a71bfb56
Riggs <riggs9162@gmx.de>
2025-04-13 18:10:20 +0200

    fix: correct model assignment syntax for Soda Can item

--- schema/items/sh_soda.lua
+++ schema/items/sh_soda.lua
@@ -3,7 +3,7 @@ local ITEM = {}
 ITEM.Name = "Soda Can"
 ITEM.Description = "A can of soda."
 ITEM.Category = "Food & Drink"
-ITEM.Model = "models/props_junk/PopCan01a.mdl"
+ITEM.Model = Model("models/props_junk/PopCan01a.mdl")
 
 ITEM.Weight = 0.5
 
