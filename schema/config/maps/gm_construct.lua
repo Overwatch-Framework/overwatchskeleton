3818c1c81ef4832d4e21d9614d897ad65e7ddaf0
Riggs <riggs9162@gmx.de>
2025-04-26 23:53:53 +0200

    Update default camera settings for gm_construct
    Changed the default camera position and angle for the gm_construct map to new coordinates.

--- schema/config/maps/gm_construct.lua
+++ schema/config/maps/gm_construct.lua
@@ -1,3 +1,3 @@
-ow.config:SetDefault("menuCamPos", Vector(-2856, -1673.71875, 76))
-ow.config:SetDefault("menuCamAng", Angle(345, 344, 0))
+ow.config:SetDefault("menuCamPos", Vector(1075.1514892578, 5697.61328125, 2065.0930175781))
+ow.config:SetDefault("menuCamAng", Angle(19.0080909729, -134.59184265137, 0))
 ow.config:SetDefault("menuCamFov", 75)
