baf34907b0c837cc3418a9a8683bb499af64b2bc
bloodycop <eon.bloodycop@gmail.com>
2025-04-27 02:17:30 +0300

    Boss, I can't handle this no more

--- schema/config/sh_config.lua
+++ schema/config/sh_config.lua
@@ -1,2 +1,2 @@
 ow.config:SetDefault("schemaColor", Color(0, 200, 75))
-ow.config:SetDefault("mainMenuMusic", "ambient/music/country_rock_am_radio_loop.wav")
+ow.config:SetDefault("mainMenuMusic", "music/vlvx_song15.mp3")
