a07a3d09a555de0d5df5c778589fd62c0532d7a0
Riggs <riggs9162@gmx.de>
2025-05-02 19:21:36 +0200

    Update item definitions and add pistol item
    Removed redundant local ITEM declarations and registration calls in sh_divorce_papers.lua and sh_soda.lua for cleaner code. Added a new item definition for a pistol in sh_wep_pistol.lua, including equip and unequip actions.

--- schema/items/sh_soda.lua
+++ schema/items/sh_soda.lua
@@ -1,10 +1,6 @@
-local ITEM = {}
-
 ITEM.Name = "Soda Can"
 ITEM.Description = "A can of soda."
 ITEM.Category = "Food & Drink"
 ITEM.Model = Model("models/props_junk/PopCan01a.mdl")
 
 ITEM.Weight = 0.5
-
-ow.item:Register("soda", ITEM)
---
+++ schema/items/sh_wep_pistol.lua
@@ -0,0 +1,38 @@
+ITEM.Name = "Pistol"
+ITEM.Description = "A standard sidearm, effective at close to medium range."
+ITEM.Category = "Weapons"
+ITEM.Model = Model("models/weapons/w_pistol.mdl")
+
+ITEM.Weight = 1.5
+
+ITEM.Actions.Equip = {
+    Name = "Equip",
+    Description = "Equip the pistol.",
+    OnRun = function(this, item, ply)
+        if ( !IsValid(ply) ) then return end
+
+        ply:Give("weapon_pistol")
+        return true
+    end,
+    OnCanRun = function(this, item, ply)
+        if ( !IsValid(ply) ) then return false end
+
+        return !ply:HasWeapon("weapon_pistol")
+    end
+}
+
+ITEM.Actions.EquipUn = {
+    Name = "Unequip",
+    Description = "Unequip the pistol.",
+    OnRun = function(this, item, ply)
+        if ( !IsValid(ply) ) then return end
+
+        ply:StripWeapon("weapon_pistol")
+        return true
+    end,
+    OnCanRun = function(this, item, ply)
+        if ( !IsValid(ply) ) then return false end
+
+        return ply:HasWeapon("weapon_pistol")
+    end
+}
